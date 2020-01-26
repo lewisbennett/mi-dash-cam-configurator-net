d035e964803a8974087b91407204b16eb582e7f3
Lewis <lewisbennett669@gmail.com>
2020-01-26 14:49:23 +0000

    Added VisibilityValueConverter

---
+++ src/MiCamConfig.App.Droid/ValueConverters/VisibilityValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using Android.Views;
+using MvvmCross.Converters;
+using System;
+using System.Globalization;
+
+namespace MiCamConfig.App.Droid.ValueConverters
+{
+    public class VisibilityValueConverter : MvxValueConverter<bool, ViewStates>
+    {
+        #region Public Methods
+        protected override ViewStates Convert(bool value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value ? ViewStates.Visible : ViewStates.Gone;
+        }
+
+        protected override bool ConvertBack(ViewStates value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == ViewStates.Visible;
+        }
+        #endregion
+    }
+}
