a7131047371a9ae56742d6cd85563f7b9b19846a
Lewis <lewisbennett669@gmail.com>
2021-04-01 23:56:57 +0100

    Moved startup logic to application instead of splash screen

--- src/MiCamConfig.App.Droid/Activities/SplashScreenActivity.cs
+++ src/MiCamConfig.App.Droid/Activities/SplashScreenActivity.cs
@@ -1,7 +1,4 @@
 ﻿using Android.App;
-using Android.OS;
-using DialogMessaging;
-using DialogMessaging.Interactions;
 using MvvmCross.Platforms.Android.Views;
 
 namespace MiCamConfig.App.Droid.Activities
@@ -9,26 +6,5 @@ namespace MiCamConfig.App.Droid.Activities
     [Activity(MainLauncher = true, NoHistory = true, Theme = "@style/AppTheme.Splash")]
     public class SplashScreenActivity : MvxSplashScreenActivity
     {
-        #region Lifecycle
-        protected override void OnCreate(Bundle bundle)
-        {
-            base.OnCreate(bundle);
-
-            ConfigureDialogMessaging();
-        }
-        #endregion
-
-        #region Private Methods
-        private void ConfigureDialogMessaging()
-        {
-            MessagingService.Init(this);
-
-            ActionSheetBottomConfigDefaults.Cancelable = true;
-
-            ActionSheetConfigDefaults.Cancelable = true;
-
-            AlertConfigDefaults.Cancelable = true;
-        }
-        #endregion
     }
 }
--- src/MiCamConfig.App.Droid/MainApplication.cs
+++ src/MiCamConfig.App.Droid/MainApplication.cs
@@ -1,5 +1,7 @@
 ﻿using Android.App;
 using Android.Runtime;
+using DialogMessaging;
+using DialogMessaging.Interactions;
 using MvvmCross.Platforms.Android.Views;
 using System;
 
@@ -8,6 +10,28 @@ namespace MiCamConfig.App.Droid
     [Application]
     public class MainApplication : MvxAndroidApplication<Setup, Core.App>
     {
+        #region Lifecycle
+        public override void OnCreate()
+        {
+            base.OnCreate();
+
+            ConfigureDialogMessaging();
+        }
+        #endregion
+
+        #region Private Methods
+        private void ConfigureDialogMessaging()
+        {
+            MessagingService.Init(this);
+
+            ActionSheetBottomConfigDefaults.Cancelable = true;
+
+            ActionSheetConfigDefaults.Cancelable = true;
+
+            AlertConfigDefaults.Cancelable = true;
+        }
+        #endregion
+
         #region Constructors
         public MainApplication(IntPtr javaReference, JniHandleOwnership transfer)
             : base(javaReference, transfer)
