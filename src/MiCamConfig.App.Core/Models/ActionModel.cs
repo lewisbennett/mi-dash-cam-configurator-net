eb90f4c14312b34ee98ef9deed0ce4d77493ea55
Lewis <lewisbennett669@gmail.com>
2021-04-01 23:21:03 +0100

    Added ActionModel designer class

--- src/MiCamConfig.App.Core/Models/ActionModel.cs
+++ src/MiCamConfig.App.Core/Models/ActionModel.cs
@@ -2,36 +2,13 @@
 
 namespace MiCamConfig.App.Core.Models
 {
-    public class ActionModel : BaseModel
+    public partial class ActionModel : BaseModel
     {
-        #region Fields
-        private string _title = string.Empty;
-        #endregion
-
         #region Properties
         /// <summary>
         /// Gets or sets the optional data payload.
         /// </summary>
         public object Data { get; set; }
-
-        /// <summary>
-        /// Gets or sets the title.
-        /// </summary>
-        public string Title
-        {
-            get => _title;
-
-            set
-            {
-                value ??= string.Empty;
-
-                if (_title.Equals(value))
-                    return;
-
-                _title = value;
-                OnPropertyChanged(nameof(Title));
-            }
-        }
         #endregion
     }
 }
---
+++ src/MiCamConfig.App.Core/Models/ActionModel.designer.cs
@@ -0,0 +1,18 @@
+﻿namespace MiCamConfig.App.Core.Models
+{
+    partial class ActionModel
+    {
+        #region Fields
+        private string _title;
+        #endregion
+
+        #region Properties
+        public string Title
+        {
+            get => _title;
+
+            set => SetProperty(ref _title, value);
+        }
+        #endregion
+    }
+}
